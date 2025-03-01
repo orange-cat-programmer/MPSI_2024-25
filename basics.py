30d7d98f0a1a57d427321fcf8b59c5e8d825af72
Victor <141457154+orange-cat-programmer@users.noreply.github.com>
2025-03-02 00:27:45 +0100

    Add files via upload

---
+++ basics.py
@@ -0,0 +1,5 @@
+import numpy as np
+samples = np.random.random(size=(100,5))
+diff = samples[:,np.newaxis,:] - samples[np.newaxis,:,:]
+distances = np.linalg.norm(diff,axis=-1)
+print(distances.shape)
