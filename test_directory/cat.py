aa21f892b8512395a5263c74d3d37d9dc85a4c77
orange-cat-programmer <sowa.szpieg@gmail.com>
2025-03-02 14:39:28 +0100

    test

--- test_directory/cat.py
+++ test_directory/cat.py
@@ -1,3 +1,9 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
-plt.figure
+if __name__ == '__main__':
+    fig, ax = plt.subplots()
+    x = np.linspace(0,10,2000)
+    y = np.sin(x*np.pi)
+    ax.plot(x,y)
+    plt.show()
