c62e6bad02b80c2de4c5bf37a76428daa32d575a
orange-cat-programmer <sowa.szpieg@gmail.com>
2025-03-12 10:57:22 +0100

    test

--- basics.py
+++ basics.py
@@ -2,5 +2,10 @@ import numpy as np
 samples = np.random.random(size=(100,5))
 diff = samples[:,np.newaxis,:] - samples[np.newaxis,:,:]
 distances = np.linalg.norm(diff,axis=-1)
-print(distances.shape)
+#print(distances.shape)
+
+from scipy.stats import norm
+
+print(1-(2*norm.cdf(-3)))
+
 
